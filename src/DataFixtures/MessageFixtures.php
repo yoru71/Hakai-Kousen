f3b773799d958e1c69f5de7832a1d615951441df
harmoP <solange.picard21@gmail.com>
2020-01-16 11:47:07 +0100

    fixture message

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -7,39 +7,39 @@ use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Forum;
 use App\Entity\User;
 use App\Entity\Subject;
+use App\Entity\Message;
 use Faker;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
-class SubjectFixtures extends Fixture implements OrderedFixtureInterface
+class MessageFixtures extends Fixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
         // Boucle de 20 itérations
-        for($i = 1; $i <= 100; $i++){
+        for($i = 1; $i <= 50; $i++){
     
             // Création d'un nouvel article
-            $newSubject = new Subject();
+            $newMessage = new Message();
         
             // Hydratation du nouvel article
-            $newSubject
-                ->setTitle($faker->sentence(3)) // Donnera article 1, article 2, etc...
+            $newMessage
+                ->setContent($faker->sentence(5)) // Donnera article 1, article 2, etc...
                 ->setPublicationDate($faker->dateTimeBetween('-1 year', 'now'))
                 ->setAuthor( $this->getReference('user' . $faker->numberBetween(1,10) ) )
-                ->setForum( $this->getReference('forum' . $faker->numberBetween(1,10) ) )
+                ->setSubject( $this->getReference('Subject' . $faker->numberBetween(1,10) ) )
             ;
         
             // Enregistrement du nouvel article auprès de Doctrine
-            $manager->persist($newSubject);
-            $this->addReference('Subject' . $i, $newSubject);
+            $manager->persist($newMessage);
+    
         }
-
         $manager->flush();
     }
-
+    
     public function getOrder()
     {
-        return 3;
+        return 4;
     }
 
 }
